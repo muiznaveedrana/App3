25d838d47b974ff46dfc08077df694101c37abbf
Muiz_Git_SourceTree <muiznaveedrana@gmail.com>
2024-08-10 13:22:47 +0100

    Add more pages

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ df = pd.read_csv("topics.csv")
 
 for index,row in df.iterrows():
     pdf.add_page()
+
+
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(100,120,120)
+    pdf.set_text_color(90,120,120)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln = 1)
     pdf.line(x1=10, y1=20, x2=200, y2=20)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
 pdf.output("output.pdf")
