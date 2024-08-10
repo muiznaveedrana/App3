8eae765fb19b9ba188340bce0d8b4f5b8c46e432
Muiz_Git_SourceTree <muiznaveedrana@gmail.com>
2024-08-10 13:18:34 +0100

    Auto Commit

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="P", unit= "mm", format="A4")
+df = pd.read_csv("topics.csv")
 
-pdf.add_page()
-
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Variables", align="L", ln = 1, border = 1)
-
+for index,row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100,120,120)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln = 1)
+    pdf.line(x1=10, y1=20, x2=200, y2=20)
 pdf.output("output.pdf")
