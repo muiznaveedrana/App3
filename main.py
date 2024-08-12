b168c927af5232f25cb464ea26c75e5318f88cae
Muiz_Git_SourceTree <muiznaveedrana@gmail.com>
2024-08-12 16:05:43 +0100

    Basically EVERTYTHING!

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit= "mm", format="A4")
+pdf.set_auto_page_break(auto = False, margin =0)
 df = pd.read_csv("topics.csv")
 
 for index,row in df.iterrows():
     pdf.add_page()
-
+    
 
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(90,120,120)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln = 1)
-    pdf.line(x1=10, y1=20, x2=200, y2=20)
-
+    pdf.line(10, 20, 200, 20)
+    pdf.ln(250)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln = 1)
+    
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        pdf.ln(250)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180,180,180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln = 1)
 pdf.output("output.pdf")
